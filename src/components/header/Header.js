f936486b7c2a18ab0555f58e85425723d34b1bf4
Oddcaio <caiokakalima10@gmail.com>
2025-08-05 17:33:49 -0300

    postando sem os arquivos de outro projeto

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,31 +1,10 @@
-import logo from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
 import './Header.css';
-import { GoTriangleDown } from "react-icons/go";
 
 
 
 function Header() {
   return (
-    <header className="header">
-      <img
-        src={logo}
-        alt="logo"
-        className="logo"
-        style={{ width: '150px', height: 'auto' }}
-      />
-      <nav className='nav-links'>
-        <Link to="/">Coachs</Link>
-        <Link to="/">Aprenda</Link>
-        <Link to="/">Ensine</Link>
-        <Link to="/">Descubra</Link>
-        <Link to="/">Explorar</Link>
-      </nav>
-      <button>
-        <Link to="/">Entrar</Link>
-        <GoTriangleDown />
-      </button>
-    </header >
+    <h1>Header</h1>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Landing Page do Atelier</h1>
+      <h1>Landing Page</h1>
     </div>
   );
 }
