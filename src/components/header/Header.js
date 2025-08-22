e3346267587a61d9067e95b0e179bb7de8f8274e
caio medeiros <caiokakalima10@gmail.com>
2025-08-22 17:03:42 -0300

    atualizando header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1 className="logo">TRYEZ</h1>
+      <h1 className="logo">TryEz</h1>
       <nav>
         <ul className="nav-list">
           <li><Link to="/coachs">Coachs</Link></li>
