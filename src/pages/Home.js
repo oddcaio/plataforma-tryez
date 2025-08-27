99bb968d845cb4d8cce8ac02a68ea3a8ba694119
caio medeiros <caiokakalima10@gmail.com>
2025-08-27 18:21:48 -0300

    working in menu

--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
-  );
-}
---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import '../home/Home.css';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Do seu setup para o sucesso.</h1>
+      <p>Apoie seus criadores favoritos.</p>
+      <p>Aprenda com os <span className="orange">melhores</span>.</p>
+      <p>Jogue junto.</p>
+      <p>Desbloqueie seu <span className="orange">potencial</span>.</p>
+      <p>Trabalhe com o que ama.</p>
+      <p>Transforme seu <span className="orange">conhecimento</span> em <span className="green">renda</span>.</p>
+      <p>Acredite.</p>
+    </div>
+  );
+}
