f3bdeece67e01ca46d28493f0367ee0054d7225a
caio medeiros <caiokakalima10@gmail.com>
2025-09-01 17:40:21 -0300

    mentoria working

---
+++ src/pages/mentoria/Mentoria.jsx
@@ -0,0 +1,11 @@
+import '../mentoria/Mentoria.css';
+
+export default function Mentpria() {
+    return (
+        <div>
+            <h1>
+                working :)
+            </h1>
+        </div>
+    );
+}
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
-
+import Mentoria from '../pages/mentoria/Mentoria';
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/mentoria" element={<Mentoria />} />
+    
+
     </Routes>
   );
 }
