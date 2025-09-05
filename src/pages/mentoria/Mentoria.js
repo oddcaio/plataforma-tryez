41ebd7cb39a2fe35816ce510f031c7c6d56bd6ac
caio medeiros <caiokakalima10@gmail.com>
2025-09-05 16:39:59 -0300

    cleber fixed 2

--- src/pages/mentoria/Mentoria.js
+++ src/pages/mentoria/Mentoria.js
@@ -11,8 +11,8 @@ export default function Mentoria() {
     const [imgIndex, setImgIndex] = useState(0);
 
     const objetosProfessores = [
-        { nome: 'João', descricao: 'Professor de Educação Física', image: img1 },
-        { nome: 'Marília', descricao: 'Professora de Pilates', image: img2 },
+        { nome: 'Cleber', descricao: 'Treinador de CSGO', image: img1 },
+        { nome: 'Marília', descricao: 'Treinadora ', image: img2 },
         { nome: 'Pedro', descricao: 'Professor de Física', image: img3 },
         { nome: 'Augusto', descricao: 'Professor de LoL', image: img4 },
     ];
